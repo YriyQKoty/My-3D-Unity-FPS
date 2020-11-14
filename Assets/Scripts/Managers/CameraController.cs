1ab18c5721ac1dc7a321ec5c9b17632f0accb8a3
YriyQKoty <ura2013afonin@gmail.com>
2020-11-14 03:09:39 +0200

    Health bar above object Implemented
    Enemy obtained responsive health bar. Health decreases if Player shot Enemy. Enemy now normally dies (cannot shot after death).

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -7,12 +7,14 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private Camera _firstViewCamera;
     [SerializeField] private Camera _thirdViewCamera;
+    public Camera currentCamera;
 
 
     private void Start()
     {
         _firstViewCamera = Camera.main;
         _thirdViewCamera = GameObject.FindGameObjectWithTag("ThirdViewCamera").GetComponent<Camera>() as Camera;
+        currentCamera = _firstViewCamera;
     }
 
     void Update()
@@ -21,11 +23,13 @@ public class CameraController : MonoBehaviour
         {
             _firstViewCamera.enabled = true;
             _thirdViewCamera.enabled = false;
+            currentCamera = _firstViewCamera;
         }
         else if (Input.GetKeyDown("2"))
         {
             _firstViewCamera.enabled = false;
             _thirdViewCamera.enabled = true;
+            currentCamera = _thirdViewCamera;
         }
     }
 }
